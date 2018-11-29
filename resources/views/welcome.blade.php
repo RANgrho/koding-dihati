aced4361814fdd2d48bc63cf64b066a88175b4e8
RANgrho <rangrho@gmail.com>
2018-11-29 22:19:12 +0700

    jancuk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel aku kamu
+                    Laravel aku kamu jancuk
                 </div>
 
                 <div class="links">
