0f0f9ab8556f4f1a5d5262a08203260e1a0f1dc5
alhaez <muh.saferagi@gmail.com>
2018-12-05 08:27:44 +0700

    buat inputan tag & databasenya blm jadi

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
       
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-9 bg-light">
             <form action="{{ route('post.store')}}" method="POST" role="form">
@@ -12,13 +12,23 @@
                     <div class="form-group">
                         <input type="hidden" class="form-control" name="author" id="author" value="{{ Auth::user()->name }}">                    
                     </div>
+
                     <div class="form-group">
                         <label for="title">Judul</label>
                         <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp" placeholder="Masukkan Judul">                    
                     </div>
+
                     <div class="form-group">
                         <label for="context">Deskripsi</label>
                         <textarea class="form-control" name="context" id="context" rows="10"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Max 3 Tags</label>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customCheck1" checked="">
+                            <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+                        </div>
                     </div>
                     {{-- @php
                         $author = Auth::user()->name;
